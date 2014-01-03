43e71100905c242c608f533dc6702f4b79296f14
Giuliano Melo <giuliano.melo@abril.com.br>
2014-01-03 17:41:48 -0200

    ingredient controller and index view

---
+++ app/controllers/ingredients.js
@@ -0,0 +1,14 @@
+/**
+ * Ingredients Controller
+ */
+var mongoose = require('mongoose'),
+	Ingredient = mongoose.model('Ingredient');
+
+exports.index = function (req, res) {
+  	Ingredient.find(function (err, ingredientList) {
+	  res.render('ingredients/index', {
+	  	title: 'Justbrew - Ingredients',
+	  	ingredients:ingredientList
+	  });
+	});
+};
